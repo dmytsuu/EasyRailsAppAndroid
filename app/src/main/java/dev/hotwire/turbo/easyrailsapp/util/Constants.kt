5e34b118f040db548bc19721f9e745f85be4b6b9
dmytsuu <grayeyed16@gmail.com>
2024-05-01 05:28:31 +0300

    update-routes

--- app/src/main/java/dev/hotwire/turbo/easyrailsapp/util/Constants.kt
+++ app/src/main/java/dev/hotwire/turbo/easyrailsapp/util/Constants.kt
@@ -1,8 +1,8 @@
 package dev.hotwire.turbo.easyrailsapp.util
 
-private const val DEVELOPMENT_URL = "http://localhost:3000"
+private const val DEVELOPMENT_URL = "http://192.168.0.101:3000"
 private const val PRODUCTION_URL  = "https://demo.rankore.uk"
 
 const val BASE_URL    = PRODUCTION_URL
 const val CURRENT_URL = "$BASE_URL/"
-const val SIGN_IN_URL = "$BASE_URL/signin"
+const val SIGN_IN_URL = "$BASE_URL/users/sign_in"
